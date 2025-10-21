7d6104fe1017ff23c63867d952f7e0aa7147e674
dev-harvey <harvrball@gmail.com>
2025-10-21 14:52:13 -0400

    Delete task confirmation modal

--- donezo/components/DeleteTaskButton.js
+++ donezo/components/DeleteTaskButton.js
@@ -1,35 +1,77 @@
 "use client";
 
 import { deleteTaskAction } from "@/lib/actions";
-import { Snackbar } from "@mui/material";
+import { Dialog, DialogActions, DialogContent, DialogTitle, Snackbar, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
-import { redirect } from "next/navigation";
 import { useState, useTransition } from "react";
 
 export default function DeleteTaskButton({ taskId }) {
   const [isPending, startTransition] = useTransition();
   const [error, setError] = useState("");
 
-  function handleDelete() {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  function handleDeleteTask() {
     startTransition(async () => {
       setError("");
       const response = await deleteTaskAction(taskId);
       if (!response.success) {
         setError(response.error);
       }
+      setDialogOpen(false);
     });
   }
-  // TODO: Add deletion confirmation modal
+
+  function handleClickOpenDialog() {
+    setDialogOpen(true);
+  }
+
+  function handleCloseDialog() {
+    setDialogOpen(false);
+    document.activeElement?.blur();
+  }
+
   return (
     <>
       <Button
         variant="contained"
         color="error"
-        onClick={handleDelete}
+        onClick={handleClickOpenDialog}
         disabled={isPending}
       >
         {isPending ? "Deleting..." : "Delete task"}
       </Button>
+      <Dialog
+        open={dialogOpen}
+        onClose={handleCloseDialog}
+      >
+        <DialogTitle
+          variant="h4"
+          component="p"
+        >
+          Are you sure you want to delete this task?
+        </DialogTitle>
+        <DialogContent>
+          <Typography>This action cannot be undone so your task will be deleted forever!</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            onClick={handleCloseDialog}
+            disabled={isPending}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={handleDeleteTask}
+            disabled={isPending}
+          >
+            {isPending ? "Deleting..." : "Delete task"}
+          </Button>
+        </DialogActions>
+      </Dialog>
       {error && (
         <Snackbar
           open={error}
